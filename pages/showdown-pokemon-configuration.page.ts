d5b6734c6d2af439058aafa8643430fe6187e590
Andrés Ayala <andresmayala@gmail.com>
2024-10-15 12:24:16 -0500

    Add ShowdownPokemonConfigurationPage and modify TeamCreationPage
    Co-authored-by: Andrés Ayala <andresmayala@gmail.com>
    Co-authored-by: Santiago Patiño Betancur <spb.patino@gmail.com>

---
+++ pages/showdown-pokemon-configuration.page.ts
@@ -0,0 +1,76 @@
+import { Page, Locator } from '@playwright/test';
+import { EvsConfig } from '../types/pokemon.type';
+
+export class ShowdownPokemonConfigurationPage {
+    readonly page: Page;
+    readonly pokemonNameInputText: Locator;
+    readonly pokemonItemInputText: Locator;
+    readonly pokemonAbilityInputText: Locator;
+    readonly remainingEvsText: Locator;
+    readonly backToTeamButton: Locator;
+    
+    private readonly BACK_TO_TEAM_BUTTON_SELECTOR        = '//button[@name = "back" and contains(., "Team")]';
+    private readonly POKEMON_NAME_INPUT_TEXT_SELECTOR    = 'input[name="pokemon"]';
+    private readonly POKEMON_ITEM_INPUT_TEXT_SELECTOR    = 'input[name="item"]';
+    private readonly POKEMON_ABILITY_INPUT_TEXT_SELECTOR = 'input[name="ability"]';
+    private readonly REMAINING_EVS_TEXT_SELECTOR         = '//div[@class = "totalev"]/em';
+
+    private readonly KEYPRESS_DELAY = 80;
+
+    constructor(page: Page) {
+        this.page                    = page;
+        this.backToTeamButton        = page.locator(this.BACK_TO_TEAM_BUTTON_SELECTOR);
+        this.pokemonNameInputText    = page.locator(this.POKEMON_NAME_INPUT_TEXT_SELECTOR);
+        this.pokemonItemInputText    = page.locator(this.POKEMON_ITEM_INPUT_TEXT_SELECTOR);
+        this.pokemonAbilityInputText = page.locator(this.POKEMON_ABILITY_INPUT_TEXT_SELECTOR);
+        this.remainingEvsText        = page.locator(this.REMAINING_EVS_TEXT_SELECTOR);
+    }
+
+    selectName = async (name: string): Promise<void> => {
+        await this.pokemonNameInputText.click();
+        await this.pokemonNameInputText.pressSequentially(name, { delay: this.KEYPRESS_DELAY });
+        const foundPokemonLink: Locator = this.page.locator(`a[data-entry="pokemon|${name}"]`);
+        await foundPokemonLink.click();
+    }
+
+    assignItem = async (item: string): Promise<void> => {
+        await this.pokemonItemInputText.click();
+        await this.pokemonItemInputText.pressSequentially(item, { delay: this.KEYPRESS_DELAY });
+        const foundPokemonItem: Locator = this.page.locator(`a[data-entry="item|${item}"]`);
+        await foundPokemonItem.click();
+    }
+
+    defineAbility = async (ability: string): Promise<void> => {
+        await this.pokemonAbilityInputText.clear();
+        await this.pokemonAbilityInputText.click();
+        await this.pokemonAbilityInputText.pressSequentially(ability, { delay: this.KEYPRESS_DELAY });
+        const foundPokemonAbility: Locator = this.page.locator(`a[data-entry="ability|${ability}"]`);
+        await foundPokemonAbility.click();
+    }
+
+    assignMoves = async (moves: Array<string>): Promise<void> => {
+        let index = 0;
+        for (const move of moves) {
+            index++;
+            const moveInputText = this.page.locator(`input[name="move${index}"]`);
+            await moveInputText.click();
+            await moveInputText.pressSequentially(move, { delay: this.KEYPRESS_DELAY });
+            const foundMove: Locator = this.page.locator(`a[data-entry="move|${move}"]`);
+            await foundMove.click();
+        }
+    }
+
+    setEvs = async (evs: EvsConfig): Promise<void> => {
+        for (const info of Object.values(evs)) {
+            const {id, value} = info;
+            const evInputText = this.page.locator(`input[name="stat-${id}"]`);
+            await evInputText.pressSequentially(value.toString(), { delay:this.KEYPRESS_DELAY });
+        }
+    }
+
+    getRemainingEvs = async (): Promise<string> => this.remainingEvsText.innerText();
+
+    clickOnBackToTeam = async () => {
+        await this.backToTeamButton.click();
+    }
+}
--- pages/showdown-team-creation.page.ts
+++ pages/showdown-team-creation.page.ts
@@ -1,34 +1,54 @@
 import { Page, Locator } from '@playwright/test';
+import { PokemonTeamFormat } from '../types/pokemon-team.type';
 
 export class ShowdownTeamCreationPage {
     readonly page: Page;
     readonly teamNameInputText: Locator
     readonly formatDropdownButton: Locator;
     readonly formatInputText: Locator;
+    readonly addPokemonButton: Locator;
+    readonly validateButton: Locator;
     
     private readonly TEAM_NAME_INPUT_TEXT_SELECTOR = 'input.teamnameedit';
     private readonly TEAM_FORMAT_BUTTON_NAME       = 'Select a format';
     private readonly TEAM_FORMAT_SEARCH_BOX_NAME   = 'Search formats';
+    private readonly ADD_POKEMON_BUTTON_NAME       = 'Add Pokémon';
+    private readonly VALIDATE_BUTTON_SELECTOR      = '//button[@name="validate"]';
     
-    private readonly KEYPRESS_DELAY = 100;
+    private readonly KEYPRESS_DELAY = 80;
 
     constructor(page: Page) {
         this.page                 = page;
         this.teamNameInputText    = page.locator(this.TEAM_NAME_INPUT_TEXT_SELECTOR);
         this.formatDropdownButton = page.getByRole('button', { name: this.TEAM_FORMAT_BUTTON_NAME });
         this.formatInputText      = page.getByPlaceholder(this.TEAM_FORMAT_SEARCH_BOX_NAME);
+        this.addPokemonButton     = page.getByRole('button', { name: this.ADD_POKEMON_BUTTON_NAME });
+        this.validateButton       = page.locator(this.VALIDATE_BUTTON_SELECTOR);
     }
 
     nameTeam = async (teamName: string) => {
         await this.teamNameInputText.fill(teamName);
     }
 
-    selectFormat = async (generation: string, name: string) => {
+    selectFormat = async (format: PokemonTeamFormat) => {
         await this.formatDropdownButton.click();
+        const {generation, name} = format;
         const formatName = `${generation} ${name}`;
         await this.formatInputText.pressSequentially(formatName, { delay: this.KEYPRESS_DELAY });
         const formatFullText = `button:has-text("[${generation}] ${name}")`;
         await this.page.locator(formatFullText).click();
     }
 
+    clickOnAddPokemon = async () => {
+        await this.addPokemonButton.click();
+    }
+
+    getTeamValidationResult = async (format:PokemonTeamFormat): Promise<string> => {
+        const {generation, name} = format;
+        const TEXT = `Your team is valid for [${generation}] ${name}.`;
+        await this.validateButton.click();
+
+        const validationResultText = await this.page.locator(`text=${TEXT}`).innerText();
+        return validationResultText;
+    }
 }
